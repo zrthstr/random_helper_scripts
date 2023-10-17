3ade447d4cda0aab74466f472a8941b455dfa59b
zrthstr <zrth1k@gmail.com>
2023-10-17 19:34:55 +0200

    now also fetch user-data, dynamic and sgs

--- AWS_IMDSv1_enum.py
+++ AWS_IMDSv1_enum.py
@@ -1,17 +1,39 @@
+# AWS_IMDSv1_enum.py - a simple recusive AWS IMDSv1 crawler
+#
+# MIT License
+# Copyright (c) 2022, zrthstr
+#
+# AWS IMDSv1 has a few quirks.
+# For some reason, unlike all other dir-entries from /latest/ do not end in a slash.
+#
+# TODO: Accommodate for some more irregularities in IMDS, for example:
+# * curl http://169.254.169.254/latest/meta-data/public-keys/ -> 0=LightsailDefaultKeyPair
+#   But then we need to continue with /latest/meta-data/public-keys/0, not 0=LightsailDefaultKeyPair.
+# * What else?
+
+
 import requests
 
-base_url = "http://169.254.169.254/latest/meta-data/"
-ret = {}
+first = ['meta-data', 'dynamic', 'user-data', 'security-groups']
+base_url = "http://169.254.169.254/latest/"
 
-def next(base):
+def recurse(base):
     for r in requests.get(base).text.splitlines():
         if r.endswith("/"):
-            next(base+r)
+            recurse(base+r)
         else:
-            f = requests.get(base + r).text
+            response = requests.get(base + r)
+            if response.status_code != 200:
+                continue
+            f = response.text
             ret[base+r]=f
 
-next(base_url)
+def print_sorted_by_alpha(ret):
+    for key, value in dict(sorted(ret.items())).items():
+        print(f"{key}:\n => {value}")
 
-for key, value in dict(sorted(ret.items())).items():
-    print(f"{key}:\n => {value}")
+for f in first:
+    ret = {}
+    print(f"[+] Trying {f}")
+    recurse(base_url + f + "/")
+    print_sorted_by_alpha(ret)
